dd78fde6fb7f957c69f3d548196616f47266072f
John Nonweiler <john.nonweiler@ukaea.uk>
2020-06-09 14:00:54 +0100

    Reduce duplicated code in Probability.java

--- src/main/java/uk/co/ramp/covid/simulation/util/Probability.java
+++ src/main/java/uk/co/ramp/covid/simulation/util/Probability.java
@@ -4,10 +4,7 @@ public class Probability {
     double p;
 
     public Probability(double p) {
-        if (p < 0.0 || p > 1.0) {
-            throw new InvalidProbabilityException("Trying to initialise a probability with p = " + p);
-        }
-        this.p = p;
+        set(p);
     }
     
     public double asDouble() { return p; }
