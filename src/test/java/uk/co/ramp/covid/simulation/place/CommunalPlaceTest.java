ea9718a70acff5fff198bdceeebfd4c74ec1a40a
bobtaylor12 <bob.annette12@gmail.com>
2020-06-09 15:56:57 +0100

    Consolidated communalPlace tests to reuse code and speed up tests

--- src/test/java/uk/co/ramp/covid/simulation/place/CommunalPlaceTest.java
+++ src/test/java/uk/co/ramp/covid/simulation/place/CommunalPlaceTest.java
@@ -19,7 +19,6 @@ public class CommunalPlaceTest extends SimulationTest {
     Time t;
     DailyStats s;
 
-
     @Before
     public void setup() throws ImpossibleWorkerDistributionException {
         populationSize = 10000;
@@ -30,164 +29,58 @@ public class CommunalPlaceTest extends SimulationTest {
     }
 
     @Test
-    public void testIsShopVisitorOpen() {
+    public void testAllPlaces() throws Exception {
         //Run for 7 days
         for (int day = 0; day < 7; day++) {
             for (int i = 0; i < 24; i++) {
                 p.timeStep(t, s);
                 t = t.advance();
-                for (Shop shop : p.getPlaces().getShops()) {
+                for (CommunalPlace place : p.getPlaces().getAllPlaces()) {
                     // Get shop opening and closing time
-                    int open = shop.getTimes().getOpen();
-                    int close = shop.getTimes().getClose();
-                    //Test if shop is open or closed
-                    if (t.getHour() >= open && t.getHour() < close) {
-                        assertTrue("Day " + day + " Time " + t.getHour() + " shop unexpectedly closed", shop.isOpen(day, t.getHour()));
-                    } else {
-                        assertFalse("Day " + day + " Time " + t.getHour() + " shop unexpectedly open", shop.isOpen(day, t.getHour()));
-                    }
+                    int open = place.getTimes().getOpen();
+                    int close = place.getTimes().getClose();
 
-                    //Test if shop is open to visitors in the next hour
-                    if (t.getHour() >= open - 1 && t.getHour() < close - 1) {
-                        assertTrue("Day " + day + " Time " + t.getHour() + " shop unexpectedly closed to visitors in next hour", shop.isVisitorOpenNextHour(t));
+                    if (place instanceof Shop || place instanceof Restaurant) {
+                        testShopOrRestaurant(place, open, close, day);
+                    } else if (place instanceof Office || place instanceof Nursery || place instanceof School || place instanceof ConstructionSite) {
+                        testOfficeHours(place, open, close, day);
+                    } else if (place instanceof Hospital){
+                        testHospital(place, day);
                     } else {
-                        assertFalse("Day " + day + " Time " + t.getHour() + " shop unexpectedly open to visitors in next hour", shop.isVisitorOpenNextHour(t));
+                        throw new Exception("Type of Place not found");
                     }
                 }
             }
         }
     }
 
-    @Test
-    public void testIsRestaurantVisitorOpen() {
-        //Run for 7 days
-        for (int day = 0; day < 7; day++) {
-            for (int i = 0; i < 24; i++) {
-                p.timeStep(t, s);
-                t = t.advance();
-                for (Restaurant restaurant : p.getPlaces().getRestaurants()) {
-                    // Get restaurant opening and closing time
-                    int open = restaurant.getTimes().getOpen();
-                    int close = restaurant.getTimes().getClose();
-                    //Test if restaurant is open or closed
-                    if (t.getHour() >= open && t.getHour() < close) {
-                        assertTrue("Day " + day + " Time " + t.getHour() + " restaurant unexpectedly closed", restaurant.isOpen(day, t.getHour()));
-                    } else {
-                        assertFalse("Day " + day + " Time " + t.getHour() + " restaurant unexpectedly open", restaurant.isOpen(day, t.getHour()));
-                    }
+    private void testShopOrRestaurant(CommunalPlace place, int open, int close, int day) {
+        //Test if shop or restaurant is open or closed
+        if (t.getHour() >= open && t.getHour() < close) {
+            assertTrue("Day " + day + " Time " + t.getHour() + " " + place.toString() + " unexpectedly closed", place.isOpen(day, t.getHour()));
+        } else {
+            assertFalse("Day " + day + " Time " + t.getHour() + " " + place.toString() + " unexpectedly open", place.isOpen(day, t.getHour()));
+        }
 
-                    //Test if restaurant is open to visitors in the next hour
-                    if (t.getHour() >= open - 1 && t.getHour() < close - 1) {
-                        assertTrue("Day " + day + " Time " + t.getHour() + " restaurant unexpectedly closed to visitors in next hour", restaurant.isVisitorOpenNextHour(t));
-                    } else {
-                        assertFalse("Day " + day + " Time " + t.getHour() + " restaurant unexpectedly open to visitors in next hour", restaurant.isVisitorOpenNextHour(t));
-                    }
-                }
-            }
+        //Test if shop or restaurant is open to visitors in the next hour
+        if (t.getHour() >= open - 1 && t.getHour() < close - 1) {
+            assertTrue("Day " + day + " Time " + t.getHour() + " " + place.toString() + " unexpectedly closed to visitors in next hour", place.isVisitorOpenNextHour(t));
+        } else {
+            assertFalse("Day " + day + " Time " + t.getHour() + " " + place.toString() + " unexpectedly open to visitors in next hour", place.isVisitorOpenNextHour(t));
         }
     }
 
-
-    @Test
-    public void testIsOfficeOpen() {
-        //Run for 7 days
-        for (int day = 0; day < 7; day++) {
-            for (int i = 0; i < 24; i++) {
-                p.timeStep(t, s);
-                t = t.advance();
-                for (Office office : p.getPlaces().getOffices()) {
-                    // Get office opening and closing time
-                    int open = office.getTimes().getOpen();
-                    int close = office.getTimes().getClose();
-                    //Test if office is open or closed
-                    if (t.getHour() >= open && t.getHour() < close && day < 5) {
-                        assertTrue("Day " + day + " Time " + t.getHour() + " office unexpectedly closed", office.isOpen(day, t.getHour()));
-                    } else {
-                        assertFalse("Day " + day + " Time " + t.getHour() + " office unexpectedly open", office.isOpen(day, t.getHour()));
-                    }
-                }
-            }
+    private void testOfficeHours(CommunalPlace place, int open, int close, int day) {
+        //Test if place is open or closed
+        if (t.getHour() >= open && t.getHour() < close && day < 5) {
+            assertTrue("Day " + day + " Time " + t.getHour() + " " + place.toString() + " unexpectedly closed", place.isOpen(day, t.getHour()));
+        } else {
+            assertFalse("Day " + day + " Time " + t.getHour() + " " + place.toString() + " unexpectedly open", place.isOpen(day, t.getHour()));
         }
     }
 
-    @Test
-    public void testIsConstructionSiteOpen() {
-        //Run for 7 days
-        for (int day = 0; day < 7; day++) {
-            for (int i = 0; i < 24; i++) {
-                p.timeStep(t, s);
-                t = t.advance();
-                for (ConstructionSite cs : p.getPlaces().getConstructionSites()) {
-                    // Get Construction Site opening and closing time
-                    int open = cs.getTimes().getOpen();
-                    int close = cs.getTimes().getClose();
-                    //Test if Construction Site is open or closed
-                    if (t.getHour() >= open && t.getHour() < close && day < 5) {
-                        assertTrue("Day " + day + " Time " + t.getHour() + " Construction Site unexpectedly closed", cs.isOpen(day, t.getHour()));
-                    } else {
-                        assertFalse("Day " + day + " Time " + t.getHour() + " Construction Site unexpectedly open", cs.isOpen(day, t.getHour()));
-                    }
-                }
-            }
-        }
-    }
-
-    @Test
-    public void testIsSchoolOpen() {
-        //Run for 7 days
-        for (int day = 0; day < 7; day++) {
-            for (int i = 0; i < 24; i++) {
-                p.timeStep(t, s);
-                t = t.advance();
-                for (School school : p.getPlaces().getSchools()) {
-                    // Get school opening and closing time
-                    int open = school.getTimes().getOpen();
-                    int close = school.getTimes().getClose();
-                    //Test if school is open or closed
-                    if (t.getHour() >= open && t.getHour() < close && day < 5) {
-                        assertTrue("Day " + day + " Time " + t.getHour() + " school unexpectedly closed", school.isOpen(day, t.getHour()));
-                    } else {
-                        assertFalse("Day " + day + " Time " + t.getHour() + " school unexpectedly open", school.isOpen(day, t.getHour()));
-                    }
-                }
-            }
-        }
-    }
-
-    @Test
-    public void testIsNurseryOpen() {
-        //Run for 7 days
-        for (int day = 0; day < 7; day++) {
-            for (int i = 0; i < 24; i++) {
-                p.timeStep(t, s);
-                t = t.advance();
-                for (Nursery nursery : p.getPlaces().getNurseries()) {
-                    // Get nursery opening and closing time
-                    int open = nursery.getTimes().getOpen();
-                    int close = nursery.getTimes().getClose();
-                    //Test if nursery is open or closed
-                    if (t.getHour() >= open && t.getHour() < close && day < 5) {
-                        assertTrue("Day " + day + " Time " + t.getHour() + " nursery unexpectedly closed", nursery.isOpen(day, t.getHour()));
-                    } else {
-                        assertFalse("Day " + day + " Time " + t.getHour() + " nursery unexpectedly open", nursery.isOpen(day, t.getHour()));
-                    }
-                }
-            }
-        }
-    }
-
-    @Test
-    public void testIsHospitalOpen() {
-        //Run for 7 days
-        for (int day = 0; day < 7; day++) {
-            for (int i = 0; i < 24; i++) {
-                p.timeStep(t, s);
-                t = t.advance();
-                for (Hospital place : p.getPlaces().getHospitals()) {
-                    // Hospitals should always be open
-                    assertTrue("Day " + day + " Time " + t.getHour() + " Hospital unexpectedly closed", place.isOpen(day, t.getHour()));
-                }
-            }
-        }
+    private void testHospital(CommunalPlace place, int day) {
+        // Hospitals should always be open
+        assertTrue("Day " + day + " Time " + t.getHour() + " Hospital unexpectedly closed", place.isOpen(day, t.getHour()));
     }
 }
