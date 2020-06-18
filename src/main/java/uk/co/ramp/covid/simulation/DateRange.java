a67c06d6ac8ec010d45706cf60ccb72bc1c0f09c
Blair Archibald <mrblairarchibald@gmail.com>
2020-06-18 12:29:18 +0100

    Move Serliaisers

--- src/main/java/uk/co/ramp/covid/simulation/DateRange.java
+++ src/main/java/uk/co/ramp/covid/simulation/DateRange.java
@@ -1,5 +1,9 @@
 package uk.co.ramp.covid.simulation;
 
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonSerializer;
+
 public class DateRange {
     final Time start;
     final Time end;
@@ -20,4 +24,18 @@ public class DateRange {
     public Time getEnd() {
         return end;
     }
+
+    public static JsonSerializer<DateRange> serializer = (src, typeOfSrc, context) -> {
+        JsonObject o = new JsonObject();
+        o.addProperty("start", src.getStart().getAbsDay());
+        o.addProperty("end", src.getEnd().getAbsDay());
+        return o;
+    };
+
+    public static JsonDeserializer<DateRange> deserializer = (json, typeOfT, context) -> {
+        JsonObject o = json.getAsJsonObject();
+        int s = o.get("start").getAsInt();
+        int e = o.get("end").getAsInt();
+        return new DateRange(Time.timeFromDay(s), Time.timeFromDay(e));
+    };
 }
--- src/main/java/uk/co/ramp/covid/simulation/parameters/ParameterIO.java
+++ src/main/java/uk/co/ramp/covid/simulation/parameters/ParameterIO.java
@@ -4,8 +4,6 @@ import com.google.gson.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import uk.co.ramp.covid.simulation.DateRange;
-import uk.co.ramp.covid.simulation.Time;
-import uk.co.ramp.covid.simulation.util.InvalidProbabilityException;
 import uk.co.ramp.covid.simulation.util.Probability;
 
 import java.io.*;
@@ -28,27 +26,8 @@ public class ParameterIO {
         Reader file = new FileReader(path);
         GsonBuilder gson = new GsonBuilder();
 
-        JsonDeserializer<Probability> pdeserializer = (json, typeOfT, context) -> {
-            double p = json.getAsDouble();
-            try {
-                return new Probability(p);
-            } catch (InvalidProbabilityException e) {
-                LOGGER.error(e);
-                // There doesn't seem to be a way to get the field name here
-                // Instead we return null and let the paramterInitiasedChecker print the error
-                return null;
-            }
-        };
-
-        JsonDeserializer<DateRange> dateRangeDeserializer = (json, typeOfT, context) -> {
-            JsonObject o = json.getAsJsonObject();
-            int s = o.get("start").getAsInt();
-            int e = o.get("end").getAsInt();
-            return new DateRange(Time.timeFromDay(s), Time.timeFromDay(e));
-        };
-
-        gson.registerTypeAdapter(Probability.class, pdeserializer);
-        gson.registerTypeAdapter(DateRange.class, dateRangeDeserializer);
+        gson.registerTypeAdapter(Probability.class, Probability.deserializer);
+        gson.registerTypeAdapter(DateRange.class, DateRange.deserializer);
         ParameterIO r = gson.create().fromJson(file, ParameterIO.class);
 
         CovidParameters.setParameters(r.disease);
@@ -58,17 +37,8 @@ public class ParameterIO {
     public static void writeParametersToFile(Path outF) {
         GsonBuilder gson = new GsonBuilder().setPrettyPrinting();
 
-        JsonSerializer<Probability> pserializer = (src, typeOfSrc, context) -> new JsonPrimitive(src.asDouble());
-
-        JsonSerializer<DateRange> dateRangeSerializer = (src, typeOfSrc, context) -> {
-            JsonObject o = new JsonObject();
-            o.addProperty("start", src.getStart().getAbsDay());
-            o.addProperty("end", src.getEnd().getAbsDay());
-            return o;
-        };
-
-        gson.registerTypeAdapter(Probability.class, pserializer);
-        gson.registerTypeAdapter(DateRange.class, dateRangeSerializer);
+        gson.registerTypeAdapter(Probability.class, Probability.serializer);
+        gson.registerTypeAdapter(DateRange.class, DateRange.serializer);
 
         ParameterIO params = new ParameterIO(CovidParameters.get(), PopulationParameters.get());
 
