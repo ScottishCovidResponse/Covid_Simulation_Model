d9cba4eaa5128ba080357561766a88d6ace6f078
Paul Bessell <prbessell@gmail.com>
2020-06-24 15:45:31 +0100

    Basic code for analysis

---
+++ RCode/CovidSimAnalysis/scripts/SourceScripts/DetahMunching.R
@@ -0,0 +1,9 @@
+deathData <- import("deathData/NRSDeathsWK23.xlsx")
+hbDeaths <- deathData[deathData$HB == healthboard,]
+hbDeaths <- cumsum(hbDeaths[-1])
+deathDays <- seq(from = 0, length.out = (ncol(deathData) - 1), by = 7)
+deathdf <- data.frame(deathDays)
+deathdf$cumDeaths <- cumsum(as.vector(unlist(hbDeaths)))
+
+deathDataCumSum <- apply(deathData[,2:ncol(deathData)], 1, function(x) cumsum(x))
+
