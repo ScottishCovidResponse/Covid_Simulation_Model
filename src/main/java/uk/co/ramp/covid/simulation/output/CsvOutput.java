28b98c8a0e69a00dfc7672af16466dc47b6f1faf
Blair Archibald <mrblairarchibald@gmail.com>
2020-07-17 11:07:47 +0100

    Extract filenames for easier changes if required

--- src/main/java/uk/co/ramp/covid/simulation/output/CsvOutput.java
+++ src/main/java/uk/co/ramp/covid/simulation/output/CsvOutput.java
@@ -14,6 +14,11 @@ import org.apache.logging.log4j.Logger;
 public class CsvOutput {
     private static final Logger LOGGER = LogManager.getLogger(CsvOutput.class);
 
+    private static final String STATS_FNAME = "out.csv";
+    private static final String INFECTIONS_OVER_TIME_FNAME = "dailyInfections.csv";
+    private static final String DEATHS_OVER_TIME_FNAME = "deaths.csv";
+    private static final String DEATHS_BY_AGE_FNAME = "deathsByAge.csv";
+
     public static void writeDailyStats(Appendable out, int startIterID, List<List<DailyStats>> stats) throws IOException {
         if (stats.isEmpty() || stats.get(0).isEmpty()) {
             return;
@@ -41,8 +46,10 @@ public class CsvOutput {
 
     public static void extraOutputsForThibaud(Path outputDir, List<List<DailyStats>> stats) {
         try {
-            CSVPrinter dailyInfectionsCSV = new CSVPrinter(new FileWriter(outputDir.resolve("dailyInfections.csv").toFile()), CSVFormat.DEFAULT);
-            CSVPrinter deathsCSV = new CSVPrinter(new FileWriter(outputDir.resolve("deaths.csv").toFile()), CSVFormat.DEFAULT);
+            CSVPrinter dailyInfectionsCSV = new CSVPrinter(
+                    new FileWriter(outputDir.resolve(INFECTIONS_OVER_TIME_FNAME).toFile()), CSVFormat.DEFAULT);
+            CSVPrinter deathsCSV = new CSVPrinter(
+                    new FileWriter(outputDir.resolve(DEATHS_OVER_TIME_FNAME).toFile()), CSVFormat.DEFAULT);
             for (int i = 0; i < stats.size(); i++) {
                 for (DailyStats s : stats.get(i)) {
                     dailyInfectionsCSV.print(s.getTotalDailyInfections());
@@ -61,7 +68,7 @@ public class CsvOutput {
     public static void writeDeathsByAge(Path outputDir, int startIter, List<List<DailyStats>> stats)  {
         final String[] headers = {"iter", "day", "age", "deaths"};
         try {
-            FileWriter file = new FileWriter(outputDir.resolve("deathsByAge.csv").toFile());
+            FileWriter file = new FileWriter(outputDir.resolve(DEATHS_BY_AGE_FNAME).toFile());
             CSVPrinter printer = new CSVPrinter(file, CSVFormat.DEFAULT.withHeader(headers));
 
             for (int i = 0; i < stats.size(); i++) {
